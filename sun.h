4dc1713e0f3ced1d76516b6b35eb533b0e8a64b2
japeral <japeralsoler@gmail.com>
2019-01-27 22:46:26 +0100

    Libraries to calculate  sunrise and sunset time based on coordinates added.

---
+++ sun.h
@@ -0,0 +1,6 @@
+
+float calculateSunrise(int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings);
+float calculateSunset (int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings);
+
+void printSunrise(int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings);
+void printSunset(int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings);
