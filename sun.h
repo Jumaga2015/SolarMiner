6caa022ead53bded6803f37feebc28ae91a9b4c2
japeral <japeralsoler@gmail.com>
2019-02-28 00:04:35 +0100

    Modified sun.c/.h library with jacobean date. Initial drop.

--- sun.h
+++ sun.h
@@ -4,3 +4,7 @@ float calculateSunset (int year,int month,int day,float lat, float lng,int local
 
 void printSunrise(int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings);
 void printSunset(int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings);
+
+double calcJD(int year,int month,int day);
+double calcSunriseUTC(double JD, double latitude, double longitude);
+double calcSunsetUTC(double JD, double latitude, double longitude);
